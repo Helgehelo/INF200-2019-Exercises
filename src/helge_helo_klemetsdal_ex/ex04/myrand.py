caf9dfdbf11c1f76963ddf67f2c024e7b3510592
Helge <helge.helo@gmail.com>
2019-10-25 10:44:24 +0200

    Added more code

--- src/helge_helo_klemetsdal_ex/ex04/myrand.py
+++ src/helge_helo_klemetsdal_ex/ex04/myrand.py
@@ -13,3 +13,25 @@ class LCGRand:
         m = 2**31-1
         random_number = a*16807*self.seed*m
         return random_number
+
+
+class ListRand:
+    def __init__(self, list_of_numbers):
+        self.list = list_of_numbers
+
+    def rand(self):
+        number_list = self.list
+        if len(number_list) == 0:
+            raise RuntimeError("Last number has been returned")
+        number = number_list.pop(0)
+        return number
+
+
+if __name__ == "__main__":
+    lcg = LCGRand(1)
+    lr = ListRand([1, 2, 3])
+    for _ in range(3):
+        print(lcg.rand())
+
+    for _ in range(3):
+        print(lr.rand())
