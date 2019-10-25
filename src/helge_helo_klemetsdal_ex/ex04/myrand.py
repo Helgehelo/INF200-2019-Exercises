0af0f7200476eb6981482a8b1edb55adecf54616
Helge <helge.helo@gmail.com>
2019-10-25 10:42:35 +0200

    Added code for myrand file

--- src/helge_helo_klemetsdal_ex/ex04/myrand.py
+++ src/helge_helo_klemetsdal_ex/ex04/myrand.py
@@ -2,3 +2,14 @@
 
 __author__ = 'Helge Helo Klemetsdal'
 __email__ = 'hegkleme@nmbu.no'
+
+
+class LCGRand:
+    def __init__(self, seed):
+        self.seed = seed
+
+    def rand(self):
+        a = 16807
+        m = 2**31-1
+        random_number = a*16807*self.seed*m
+        return random_number
